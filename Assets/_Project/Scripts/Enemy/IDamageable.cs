8bad9cb3b41b2aaaa25c71a2879ada8fcff63025
TheOctan <kanatush.tim@gmail.com>
2024-07-25 11:51:51 +0300

    Auto aim

---
+++ Assets/_Project/Scripts/Enemy/IDamageable.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace OctanGames.Enemy
+{
+    public interface IDamageable
+    {
+        void TakeHit(float damage, Vector3 hitPoint, Vector3 hitDirection);
+        void TakeDamage(float damage);
+    }
+}
--- Assets/_Project/Scripts/Player/PlayerAim.cs
+++ Assets/_Project/Scripts/Player/PlayerAim.cs
@@ -1,15 +1,25 @@
-﻿using OctanGames.Services.Input;
+﻿using System;
+using System.Collections;
+using System.Linq;
+using OctanGames.Services.Input;
 using UnityEngine;
 
 namespace OctanGames.Player
 {
     public class PlayerAim : MonoBehaviour
     {
+        private const float AIM_DELAY = 1f;
+
         [SerializeField] private float _aimRadius = 5f;
         [SerializeField] private float _turnSpeed = 12f;
+        [SerializeField] private LayerMask _mask;
 
         private IInputService _inputService;
         private Vector3 _targetDirection;
+
+        private readonly Collider2D[] _aimTargets = new Collider2D[5];
+        private Transform _currentAimTarget;
+        private int _lastHitCount;
 
         public PlayerAim Construct(IInputService inputService, float aimRadius)
         {
@@ -19,11 +29,46 @@ namespace OctanGames.Player
             return this;
         }
 
+        private void OnEnable()
+        {
+            StartCoroutine(FindNearestTarget());
+        }
+
         public void FixedUpdate()
         {
-            _targetDirection = _inputService.Axis;
+            _targetDirection = _currentAimTarget != null
+                ? _currentAimTarget.position - transform.position
+                : _inputService.Axis;
 
             PlayerRotate(_targetDirection);
+        }
+
+        private IEnumerator FindNearestTarget()
+        {
+            while (enabled)
+            {
+                Array.Clear(_aimTargets, 0, _aimTargets.Length);
+                int hitCount = Physics2D.OverlapCircleNonAlloc(transform.position, _aimRadius, _aimTargets, _mask);
+
+                if (hitCount > 0)
+                {
+                    _currentAimTarget = _aimTargets
+                        .Where(e => e != null)
+                        .OrderBy(e => (transform.position - e.transform.position).sqrMagnitude)
+                        .FirstOrDefault()
+                        ?.transform;
+                }
+                else
+                {
+                    _currentAimTarget = null;
+                }
+
+                _lastHitCount = hitCount;
+
+                yield return new WaitForSeconds(AIM_DELAY);
+            }
+
+            _currentAimTarget = null;
         }
 
         private void PlayerRotate(Vector3 direction)
@@ -31,7 +76,7 @@ namespace OctanGames.Player
             if (direction == Vector3.zero) return;
 
             Quaternion lookRotation = Quaternion.LookRotation(Vector3.forward, direction);
-            Quaternion targetRotation = Quaternion.Euler(0,0, lookRotation.eulerAngles.z);
+            Quaternion targetRotation = Quaternion.Euler(0, 0, lookRotation.eulerAngles.z);
             float turnStep = _turnSpeed * Time.fixedDeltaTime;
 
             transform.rotation = AccelerateRotation(transform.rotation, targetRotation, turnStep);
